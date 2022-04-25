92ff10915eb9d57beb2b7f313ae317d803d47550
aztekinbeytullah@gmail.com <aztekinbeytullah@gmail.com>
2022-04-25 13:57:59 +0300

    ...

--- WebIU/Areas/Admin/Controllers/ExerciseController.cs
+++ WebIU/Areas/Admin/Controllers/ExerciseController.cs
@@ -22,19 +22,38 @@ namespace WebIU.Areas.Admin.Controllers
             return View(exercises);
         }
 
-        public  IActionResult AddExercise()
+        public  IActionResult Add()
         {
-
             return View();
         }
 
         [HttpPost]
-        public IActionResult AddExercise(Exercise exercise)
+        public IActionResult Add(Exercise exercise)
         {
-
+            exercise.CreatedTime = DateTime.Now;
+            _exerciseService.Add(exercise);
             return View();
         }
 
+        public IActionResult Remove(int id)
+        {
+            var result = _exerciseService.GetById(id);
+            _exerciseService.Remove(result);
+            return View("Index");
+        }
+        
+        public IActionResult Update(int id)
+        {
+           var result=_exerciseService.GetById(id);
+           return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Exercise exercise)
+        {
+            _exerciseService.Update(exercise);
+            return View("Index");
+        }
 
 
 
