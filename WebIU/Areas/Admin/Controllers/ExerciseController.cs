11772558cdbcbefdcb3463fb5f75dedeb663b67b
aztekinbeytullah@gmail.com <aztekinbeytullah@gmail.com>
2022-04-27 15:22:31 +0300

    ...

--- WebIU/Areas/Admin/Controllers/ExerciseController.cs
+++ WebIU/Areas/Admin/Controllers/ExerciseController.cs
@@ -11,10 +11,14 @@ namespace WebIU.Areas.Admin.Controllers
     {
         IExerciseServices _exerciseService;
         ICategoryServices _categoryService;
-        public ExerciseController(IExerciseServices exerciseService, ICategoryServices categoryServices)
+        IQuestionServices _questionService;
+        public ExerciseController(  IExerciseServices exerciseService,
+                                    ICategoryServices categoryServices,
+                                    IQuestionServices questionServices)
         {
             _exerciseService=exerciseService;
             _categoryService = categoryServices;
+            _questionService = questionServices;
         }
 
         public IActionResult Index()
@@ -35,13 +39,24 @@ namespace WebIU.Areas.Admin.Controllers
         {
             exercise.CreatedTime = DateTime.Now;
             _exerciseService.Add(exercise);
-            
+            AssingQuestionToExercise(exercise);
 
             return RedirectToAction("Index", "Exercise");
           
         }
 
-        
+        private void AssingQuestionToExercise(Exercise e)
+        {
+            var result=_questionService.GetList().Where(x => x.CategoryId ==e.CategoryId).ToList();
+            Random rnd = new Random();
+            List<Question> selectedQuestions = new List<Question>();
+            for (int i = 0; i < e.QuestionCount-1; i++)
+            {
+               selectedQuestions.Add(result[rnd.Next(0, result.Count)]);
+            }
+            //Seçilen sorular ilgili tabloya işlenmesi gerekiyor.
+
+        }
 
         public IActionResult Remove(int id)
         {
